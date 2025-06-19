601da40b3ed4558abcf112cd0b892bfc580fb7a8
CHAUDHARY_SUHANA <suhanachaudhary212@gmail.com>
2025-06-19 14:44:50 +0530

    Footer modifications

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,7 +3,7 @@ const Footer = () => {
   return (
     <footer className="bg-[#ff7311] text-center text-black py-12 mt-auto">
       <p className="text-lg font-normal">
-        Copyright © 2024 Proswi Technology Private Limited - All Rights Reserved.
+        Copyright © 2025 Paranjal Technology Private Limited - All Rights Reserved.
       </p>
     </footer>
   );
